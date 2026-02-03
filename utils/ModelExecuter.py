771abe342e655f8d1d179e5a3bd5c25d573e6a9e
Alvin <99197625+IalvinchangI@users.noreply.github.com>
2026-02-03 18:04:30 +0800

    feat: add utils
    1. add ModelExecuter (not implemented)
    2. add ModelLoader (not implemented)

---
+++ utils/ModelExecuter.py
@@ -0,0 +1,3 @@
+
+class ModelExecuter():
+    pass
---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+from .ModelLoader import ModelLoader
+from .ModelExecuter import ModelExecuter
+
+__all__ = [
+    "ModelLoader", 
+    "ModelExecuter"
+]
